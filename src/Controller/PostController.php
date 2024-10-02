c86e23faf6162b35585be6571edba6d428bafc31
Jean Demel <demeljean@hotmail.fr>
2024-10-02 12:31:48 +0200

    fix + find by user

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 use App\Entity\Post;
 use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/post')]
@@ -20,12 +22,28 @@ class PostController extends AbstractController {
         return $this->json($post, 201);
     }
 
+    //http://localhost:8000/api/post?page=...
     #[Route(methods:'GET')]
     public function all(#[MapQueryParameter] int $page = 1) {
         
 
         return $this->json(
             $this->repo->findAll(($page-1)*15, 15)
+        );
+    }
+    //http://localhost:8000/api/post/user/test?page=....
+    #[Route('/user/{username}',methods:'GET')]
+    public function byAuthor(
+        string $username, 
+        UserRepository $userRepo, 
+        #[MapQueryParameter] int $page = 1) {
+        //Pourquoi pas vérifier si le user existe avant de récupérer ses posts, pour renvoyer un 404 si jamais c'est pas le cas
+        if(!$userRepo->findByIdentifier($username)) {
+            throw new NotFoundHttpException('User does not exists');
+        }
+
+        return $this->json(
+            $this->repo->findByUsername($username,($page-1)*15, 15)
         );
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -22,7 +22,7 @@ class PostRepository {
 
     public function findAll(int $offset, int $limit) {
         $connection = Database::connect();
-        $query = $connection->prepare("SELECT * FROM post INNER JOIN user ON user.id=post.author_id ORDER BY posted_at DESC LIMIT $offset,$limit ");
+        $query = $connection->prepare("SELECT *, post.id post_id FROM post INNER JOIN user ON user.id=post.author_id ORDER BY posted_at DESC LIMIT $offset,$limit ");
         $query->execute();
 
         $list= [];
@@ -36,7 +36,43 @@ class PostRepository {
             $user->setBanDate($line['ban_date']);
             
             $post = new Post();
-            $post->setId($line['id']);
+            $post->setId($line['post_id']);
+            $post->setPostedAt(new DateTimeImmutable($line['posted_at']));
+            $post->setContent($line['content']);
+            $post->setAuthor($user);
+            // $post->setRespondTo(null)
+            
+            
+            
+            $list[] = $post;
+        }
+        return $list;
+    }
+    /**
+     * Méthode pour récupérer tous les posts d'un User par son username
+     * @param string $username le username de la personne dont on veut récupérer les posts
+     * @param int $offset combien on saute de posts
+     * @param int $limit combien on récupère de posts
+     * @return Post[] Les posts de la personne
+     */
+    public function findByUsername(string $username,int $offset, int $limit) {
+        $connection = Database::connect();
+        $query = $connection->prepare("SELECT *, post.id post_id FROM post INNER JOIN user ON user.id=post.author_id WHERE user.username=:username ORDER BY posted_at DESC LIMIT $offset,$limit ");
+        $query->bindValue(':username', $username);
+        $query->execute();
+
+        $list= [];
+        foreach($query->fetchAll() as $line) {
+            $user = new User();
+            $user->setId($line['author_id']);
+            $user->setUsername($line['username']);
+            $user->setEmail($line['email']);
+            $user->setCreatedAt(new DateTimeImmutable($line['created_at']));
+            $user->setRole($line['role']);
+            $user->setBanDate($line['ban_date']);
+            
+            $post = new Post();
+            $post->setId($line['post_id']);
             $post->setPostedAt(new DateTimeImmutable($line['posted_at']));
             $post->setContent($line['content']);
             $post->setAuthor($user);
